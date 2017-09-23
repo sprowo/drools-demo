cf0eb54da167d7c351aa5830fbc5dda0b08f0b53
linan <linan@f-road.com.cn>
2017-09-23 11:34:06 +0800

    dynamic fix

---
+++ dynamic-string-demo/src/main/java/com/prowo/Person.java
@@ -0,0 +1,48 @@
+package com.prowo;
+
+/**
+ * Created by linan on 2017/8/16.
+ */
+public class Person {
+    private String name;
+    private int age;
+    private String desc;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", desc='" + desc + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
